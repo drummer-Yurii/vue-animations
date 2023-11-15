52f1cebe1aea2b9fac2617c4b3f29acb9873568f
Yurii Rybachok <drummer2009@ukr.net>
2023-11-15 16:54:31 +0200

    add nav for change page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
 
 <template>
   <div>
-    <router-view></router-view>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view v-slot="{ Component }">
+      <transition>
+        <Component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
